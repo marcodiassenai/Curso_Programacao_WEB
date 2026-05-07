62481dc8a1622d5ea4ef7925b6485e58b9f30ceb
marcodiassenai <marcodiassenai@gmail.com>
2026-05-07 19:03:47 -0300

    javafeito

--- Lista_de_Tarefa_Prof/js/script.js
+++ Lista_de_Tarefa_Prof/js/script.js
@@ -1,19 +1,33 @@
 function adicionarTarefa() {
 
     //recebe valor do input do usuário
-    let inputTarefa = document.getElementById("inputTarefa").value
+    //usar .trim() para remover espaços e testar se sobrou algum caractere
+    let inputTarefa = document.getElementById("inputTarefa").value.trim();
     let tarefa = inputTarefa
     console.log(inputTarefa)
 
+    // valida se está vazio
+    if (tarefa === "") {
+        document.getElementById("mensagem").textContent =
+            "Digite uma tarefa antes de adicionar!"
+        return
+    }
+
     //cria novo item (li) e insere na (lista ul)
-   let listaTarefas = document.getElementById("listaTarefas")
-   let novaTarefa = document.createElement("li")
+    let listaTarefas = document.getElementById("listaTarefas")
+    let novaTarefa = document.createElement("li")
     novaTarefa.textContent = tarefa
-   listaTarefas.appendChild(novaTarefa)
+    listaTarefas.appendChild(novaTarefa)
 
     //mensagem de tarefa adicionada com sucesso
     let mensagem = "Tarefa adicionada com sucesso!"
     document.getElementById("mensagem").textContent = mensagem
     //limpa o input do usuário
     inputTarefa.value = ""
+
+    if (novaTarefatarefa.trim() !== "") 
+    tarefa.innerText = campo.value;
+        else 
+    mensagem.innerText = "";
+
 }
