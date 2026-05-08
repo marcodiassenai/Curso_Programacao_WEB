a13818a8187d433b73d896a5932b18a4703851bc
marcodiassenai <marcodiassenai@gmail.com>
2026-05-07 22:16:58 -0300

    2

--- Lista_de_Tarefa_Prof/js/script.js
+++ Lista_de_Tarefa_Prof/js/script.js
@@ -7,9 +7,15 @@ function adicionarTarefa() {
     //value = para querer somente o comando que ta escrito dentro da caixa
     //o let é para criar uma variável dentro do javascript, e nela colocar o valor que esta em inputTarefa
     let inputTarefa = document.getElementById("inputTarefa").value.trim();
+    
+    //cria uma variavel para pegar o que esta dentro do id mensagem do html
+    //textContent = diz para a variavel msn que é para colocar o texto que esta entre aspas
+    let msn = document.getElementById("mensagem").textContent = "Tarefa adicionada com sucesso"
+    
+    //variavel tarefa recebe o que esta dentro da variavel inputTarefa criada acima
     let tarefa = inputTarefa
 
-    //mostra o que ta sendo escrito //
+    //mostra o que ta sendo escrito, semelhando ao comando 'escreva' do visualg 
     console.log(inputTarefa)
 
     // valida se está vazio
