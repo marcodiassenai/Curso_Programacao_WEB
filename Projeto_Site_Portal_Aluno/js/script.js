619926d1a4003731c35c40bdeb09fde38c746f0f
marcodiassenai <marcodiassenai@gmail.com>
2026-05-11 20:40:45 -0300

    atualizado

--- Projeto_Site_Portal_Aluno/js/script.js
+++ Projeto_Site_Portal_Aluno/js/script.js
@@ -2,23 +2,44 @@
 function adicionarTarefa() {
 
     //criei uma variavel 'nomeProdutoVar' que vai pegar o valor digitado site Nome do Prduto
-    let produto = document.getElementById("nome_Produto").value
+    let produto = document.getElementById("nome_Produto").value.trim()
     console.log(produto)
-   
-
-    let msgCadastro = document.getElementById("msg_Cadastro").textContent = "Produto adicionado a Lista"
+       
     
+    let msgCadastro = document.getElementById("msg_Cadastro").textContent = "Produto adicionado a Lista"
+
+    //faz com que o campo do input volte a ficar vazio apos cadastrar o produto
+    document.getElementById("nome_Produto").value = ""
     console.log(produto)
+
+
+    // apaga a mensagem após 2 segundos
+    setTimeout(() => { 
+    msg_Cadastro.textContent = ""
+    }, 2000);
+    
+
+
+    //comando 'if' faz com que se o campo do produto estiver vazio ele não adiciona e aparece uma mensagem informando que é para adiconar um produto
+    if (produto === "") {
+        document.getElementById("msg_Cadastro").textContent =
+            "Digite um Produto para adicionar!"
+        return
+    }
+
+
 
     let listaProduto = document.getElementById("listaProduto")
+    
 
     let novoProduto = document.createElement("li")
     novoProduto.textContent = produto
     listaProduto.appendChild(novoProduto)
 
+  
     //let novoProduto = "Produto adicionado a Lista"
     let mensagem = "Produto adicionado a Lista"
     document.getElementById("msgCadastro").textContent = mensagem
-    produto.value = ""
+ 
 
-} 
+}
