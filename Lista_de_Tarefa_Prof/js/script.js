d2ead1f7d16e0ebce15bb0655172f79161f2dc05
marcodiassenai <marcodiassenai@gmail.com>
2026-05-19 18:54:01 -0300

    1

--- Lista_de_Tarefa_Prof/js/script.js
+++ Lista_de_Tarefa_Prof/js/script.js
@@ -6,15 +6,13 @@ function adicionarTarefa() {
     //usar .trim() para remover espaços e testar se sobrou algum caractere
     //value = para querer somente o comando que ta escrito dentro da caixa
     //o let é para criar uma variável dentro do javascript, e nela colocar o valor que esta em inputTarefa
-    let inputTarefa = document.getElementById("inputTarefa").value.trim();
+    let tarefa = document.getElementById("inputTarefa").value.trim();
     
     //cria uma variavel para pegar o que esta dentro do id mensagem do html
     //textContent = diz para a variavel msn que é para colocar o texto que esta entre aspas
     let msn = document.getElementById("mensagem").textContent = "Tarefa adicionada com sucesso"
     
-    //variavel tarefa recebe o que esta dentro da variavel inputTarefa criada acima
-    let tarefa = inputTarefa
-
+    
     //mostra o que ta sendo escrito, semelhando ao comando 'escreva' do visualg 
     console.log(inputTarefa)
 
@@ -30,15 +28,25 @@ function adicionarTarefa() {
 
     //"li" referece a lista, indica que a variavel novaTarefa vai receber uma lista
     let novaTarefa = document.createElement("li")
+
+    //pegue a variavel novaTarefa e adiciona o que foi escrito no input
     novaTarefa.textContent = tarefa
+    
+    //adiciona uma nova variavel que foi digitada em 'tarefa' e adiciona abaixo na lista
     listaTarefas.appendChild(novaTarefa)
 
     //mensagem de tarefa adicionada com sucesso
     let mensagem = "Tarefa adicionada com sucesso!"
+
     document.getElementById("mensagem").textContent = mensagem
+
     //limpa o input do usuário
     inputTarefa.value = ""
 
+    document.getElementById("tarefa").value = ""
+
+
+    // ????
     if (novaTarefatarefa.trim() !== "") 
     tarefa.innerText = campo.value;
         else 
