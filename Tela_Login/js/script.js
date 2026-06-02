65b7026d01938a2bdb857b26422eb497e69d6d5f
marcodiassenai <marcodiassenai@gmail.com>
2026-06-02 20:08:04 -0300

    -

--- Tela_Login/js/script.js
+++
@@ -1,6 +0,0 @@
-function botaoEntrar() {
-    let input = document.getElementById("digiteUsuario");
-    let mensagem = input.value;
-    // let mensagem = input.value
-    document.getElementById("mensagem").value. = "admin"       
-}
